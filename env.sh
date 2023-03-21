75d1a5e5e592d7f87c740d454757e2e5e1c03f40
PasLoin <raphael0arnould@gmail.com>
2023-03-21 03:40:26 +0000

    add env.sh

---
+++ env.sh
@@ -0,0 +1,12 @@
+export REGION=belgium
+export REGION_PATH=europe/$REGION
+
+export PGHOST=postgres
+export PGPORT=5432
+export PGUSER=postgres
+export PGPASSWORD=postgres
+export PGDATABASE=landuse
+# Memory in MB for osm2pgsql
+export MEMORY=4000
+
+export MAPROULETTE_API_KEY=TODO
